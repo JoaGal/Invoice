9742d2b856fe50d6e7f19f2fd50abd8d80446001
JoaGal <joaquingaldeano7@gmail.com>
2024-02-29 14:44:37 -0300

    Delete Product

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ const app = express();
 const port = process.env.DB_PORT;
 const cors = require("cors");
 const userRoutes = require("./src/routes/users");
-const productsRoutes = require("./src/routes/products");
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRoutes);
-app.use("/products", productsRoutes);
 
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
