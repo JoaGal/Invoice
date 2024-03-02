d0b60c4d444d06c026ea753cfa03d156fe81e485
JoaGal <joaquingaldeano7@gmail.com>
2024-03-02 17:50:45 -0300

    Edit styles

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -14,14 +14,14 @@ function Login() {
 
   return (
     <div>
-      <div className="dark:bg-[#141625] scrollbar-hide duration-300 min-h-screen bg-[#f8f8fb] py-[34px] px-2 md:px-8 lg:px-12 lg:py-[72px]  ">
+      <div className="dark:bg-[#141625] flex md:items-center justify-center scrollbar-hide duration-300 min-h-screen bg-[#f8f8fb] py-[34px] px-2 md:px-8 lg:px-12 lg:py-[72px]  ">
         <motion.div
           key={location.pathname}
           initial={{ x: "0" }}
           animate={{ x: 0 }}
           exit={{ x: "-150%" }}
           transition={{ duration: 0.5 }}
-          className="   max-w-3xl flex flex-col   mx-auto my-auto"
+          className=" max-w-3xl flex flex-col w-full"
         >
           {/* Center Header */}
           <div className="flex md:items-center  duration-10 rounded-lg w-full">
